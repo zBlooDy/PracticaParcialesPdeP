63c88adb54b5311d3218ab07728c7af65a9c44c4
Thiago Perez Giacchetta <tperezgiacchetta@frba.utn.edu.ar>
2024-05-18 11:20:26 -0300

    Terminado punto 1 y 2

--- HeroesLeyenda.hs
+++ HeroesLeyenda.hs
@@ -1,3 +1,4 @@
+
 -- Punto 1) ======================
 
 data Artefacto = Artefacto {
@@ -13,21 +14,36 @@ data Heroe = Heroe {
 
 -- Punto 2) ================================
 
-cambiaEpiteto nuevoEpiteto heroe = heroe {epiteto = nuevoEpiteto} 
+cambiaEpiteto :: String -> Heroe -> Heroe
+cambiaEpiteto nuevoEpiteto  = mapEpiteto (const nuevoEpiteto)
 
-agregaArtefacto nuevoArtefacto rareza heroe = heroe {artefactos = artefactos heroe ++ [Artefacto nuevoArtefacto rareza] }
+mapEpiteto :: (String -> String) -> Heroe -> Heroe
+mapEpiteto f unHeroe = unHeroe {epiteto = f $ epiteto unHeroe}
 
+agregaArtefacto :: Artefacto -> Heroe -> Heroe
+agregaArtefacto nuevoArtefacto = mapArtefacto (nuevoArtefacto :) 
+
+mapArtefacto :: ([Artefacto] -> [Artefacto]) -> Heroe -> Heroe
+mapArtefacto f unHeroe = unHeroe {artefactos = f $ artefactos unHeroe} 
+
+
+sube = Artefacto {
+    nombre = "La poderosa Sube",
+    rareza = 9000
+}
 
 rocko = Heroe {
     epiteto = "Rockito",
-    reconocimiento = 90,
-    artefactos = []
-} 
+    reconocimiento = 700,
+    artefactos = [sube]
+}
 
+paseALaHistoria :: Heroe -> Heroe
 paseALaHistoria unHeroe 
-    | reconomientoDeHeroe > 1000 = cambiaEpiteto "El mitico" unHeroe
-    | reconomientoDeHeroe >= 500  = cambiaEpiteto "El magnifico" $ agregaArtefacto "Lanza Del Olimpo" 100 unHeroe
-    | reconomientoDeHeroe > 100  = cambiaEpiteto "Hoplita" $ agregaArtefacto "Xiphos" 50 unHeroe
+    | reconomientoDeHeroe > 1000    = cambiaEpiteto "El mitico" unHeroe
+    | reconomientoDeHeroe >= 500    = cambiaEpiteto "El magnifico" . agregaArtefacto (Artefacto "Lanza Del Olimpo" 100) $ unHeroe
+    | reconomientoDeHeroe > 100     = cambiaEpiteto "Hoplita" . agregaArtefacto (Artefacto "Xiphos" 50)  $ unHeroe
     | otherwise                     = unHeroe
     where
         reconomientoDeHeroe = reconocimiento unHeroe
+
