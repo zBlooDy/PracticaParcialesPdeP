f096f6c296c53085e14686df21454442dd2ee740
Thiago Perez Giacchetta <tperezgiacchetta@frba.utn.edu.ar>
2024-05-22 16:55:06 -0300

    Punto 2

--- Series.hs
+++ Series.hs
@@ -31,3 +31,57 @@ esProblematica unaSerie = (>3) . length . actoresConMasDeUnaRestriccion $ actore
 actoresConMasDeUnaRestriccion :: [Actor] -> [Actor]
 actoresConMasDeUnaRestriccion  = filter ((>1) . length . restricciones)
 
+-------------
+-- Punto 2 --
+-------------
+type Produccion = Serie -> Serie
+
+conFavoritismos :: [Actor] -> Produccion
+conFavoritismos listaFavoritos = agregaActores listaFavoritos . sacarActores 2 
+
+sacarActores :: Int -> Serie -> Serie
+sacarActores cantidad = mapActores (drop cantidad)
+
+agregaActores :: [Actor] -> Serie -> Serie
+agregaActores listaActores = mapActores (listaActores ++)
+
+mapActores :: ([Actor] -> [Actor]) -> Serie -> Serie
+mapActores f unaSerie = unaSerie {actores = f $ actores unaSerie}
+
+johnnydepp :: Actor
+johnnydepp = Actor{
+    nombreActor = "Johnny Depp",
+    sueldoAnual = 20000000,
+    restricciones = []
+}
+
+helenabonham :: Actor
+helenabonham = Actor {
+    nombreActor = "Helena Bonham",
+    sueldoAnual = 15000000,
+    restricciones = []
+}
+
+timBurton :: Produccion
+timBurton = conFavoritismos [johnnydepp, helenabonham]
+
+gatoPardeitor :: Produccion
+gatoPardeitor unaSerie = unaSerie 
+
+estireitor :: Produccion
+estireitor = duplicarTemporadas
+
+duplicarTemporadas :: Serie -> Serie
+duplicarTemporadas = mapTemporadas (*2)
+
+mapTemporadas :: (Int -> Int) -> Serie -> Serie
+mapTemporadas f unaSerie = unaSerie {cantidadTemporadas = f $ cantidadTemporadas unaSerie}
+
+
+desespereitor :: [Produccion] -> Produccion
+desespereitor listaProducciones unaSerie = foldl (\x f -> f x) unaSerie listaProducciones
+
+canceleitor :: Float -> Produccion
+canceleitor cifra unaSerie 
+    | estaEnRojo unaSerie || ratingPromedio unaSerie < cifra  = unaSerie {cancelada = True}
+    | otherwise                                               = unaSerie
