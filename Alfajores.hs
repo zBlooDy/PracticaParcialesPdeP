8e94b80199b6129df1849405671945391d2ec569
Thiago Perez Giacchetta <tperezgiacchetta@frba.utn.edu.ar>
2024-05-25 10:50:36 -0300

    Parte 2

--- Alfajores.hs
+++ Alfajores.hs
@@ -1,8 +1,7 @@
-import Distribution.Simple.Program.HcPkg (list)
 
------------
---Parte 1--
------------
+---------------
+----Parte 1----
+---------------
 
 data Alfajor = Alfajor {
     nombre :: String,
@@ -59,3 +58,61 @@ esPotable unAlfajor = (not. null . relleno $ unAlfajor) && todasCapasIguales (re
 
 todasCapasIguales :: [Relleno] -> Bool
 todasCapasIguales listaRellenos = all (head listaRellenos ==) listaRellenos
+
+
+
+---------------
+----Parte 2----
+---------------
+
+-- <- Abaratar -> 
+abaratarAlfajor :: Alfajor -> Alfajor
+abaratarAlfajor = reducirPeso 10 . bajarDulzor 7 
+
+reducirPeso :: Float -> Alfajor -> Alfajor
+reducirPeso unPeso = mapPeso (subtract unPeso)
+
+mapPeso :: (Float -> Float) -> Alfajor -> Alfajor
+mapPeso f unAlfajor = unAlfajor {peso = f $ peso unAlfajor}
+
+bajarDulzor :: Float -> Alfajor -> Alfajor
+bajarDulzor unDulzor = mapDulzor (subtract unDulzor)
+
+mapDulzor :: (Float -> Float) -> Alfajor -> Alfajor
+mapDulzor f unAlfajor = unAlfajor {dulzor = f $ dulzor unAlfajor}
+
+-- <- Renombrar -> 
+renombrarAlfajor :: String -> Alfajor -> Alfajor
+renombrarAlfajor nuevoNombre = mapNombre (const nuevoNombre)
+
+mapNombre :: (String -> String) -> Alfajor -> Alfajor
+mapNombre f unAlfajor = unAlfajor {nombre = f $ nombre unAlfajor}
+
+-- <- Agregar capa -> 
+agregarCapaDeRelleno :: Relleno -> Alfajor -> Alfajor
+agregarCapaDeRelleno unaCapa unAlfajor = unAlfajor {relleno = unaCapa : relleno unAlfajor}
+
+-- <- Hacer Premium ->
+hacerPremium :: Alfajor -> Alfajor
+hacerPremium unAlfajor
+    | esPotable unAlfajor = nombrePremium . agregarCapaDeRelleno (head.relleno $ unAlfajor) $ unAlfajor
+    | otherwise           = unAlfajor
+
+nombrePremium :: Alfajor -> Alfajor
+nombrePremium = mapNombre (++ "premium")
+
+
+hacerPremiumVariasVeces :: Int -> Alfajor -> Alfajor
+hacerPremiumVariasVeces 0 unAlfajor = unAlfajor
+hacerPremiumVariasVeces veces unAlfajor = hacerPremiumVariasVeces (veces-1) (hacerPremium unAlfajor)
+
+
+jorgitito :: Alfajor
+jorgitito = renombrarAlfajor "Jorgitito" . abaratarAlfajor $ jorgito
+
+jorgelin :: Alfajor
+jorgelin = renombrarAlfajor "Jorgelin" . agregarCapaDeRelleno DulceDeLeche $ jorgito
+
+capitanDelEspacioCostaACosta :: Alfajor
+capitanDelEspacioCostaACosta = renombrarAlfajor "Capitán del espacio de costa a costa" . hacerPremiumVariasVeces 4 . abaratarAlfajor $ capitanDelEspacio
+
