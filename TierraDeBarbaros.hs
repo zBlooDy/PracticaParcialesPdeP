0231c9618c36c231e8a36df62bc1aa68138c5c23
Thiago Perez Giacchetta <tperezgiacchetta@frba.utn.edu.ar>
2024-05-29 16:18:25 -0300

    Punto 3
    Former-commit-id: 19029fbdb7bd34a999c89bbd1ae395cd1fd7098b

--- TierraDeBarbaros.hs
+++ TierraDeBarbaros.hs
@@ -1,4 +1,4 @@
-import Data.Char (toUpper)
+import Data.Char (toUpper, isUpper)
 import Text.Show.Functions
 
 data Barbaro = Barbaro {
@@ -59,4 +59,72 @@ concatenarPlus lista = [concat lista]
 
 megafonoBarbarico :: Objeto
 megafonoBarbarico = cuerda ardilla megafono
+
+-----------
+--Punto 3--
+-----------
+type Evento = Barbaro -> Bool
+type Aventura = [Evento]
+
+-- 1)
+
+tieneHabilidad :: String -> Barbaro -> Bool
+tieneHabilidad habilidad = elem habilidad . habilidades
+
+invasionDeSuciosDuendes :: Evento
+invasionDeSuciosDuendes  = tieneHabilidad "Escribir Poesia Atroz"  
+
+-- 2)
+
+cremalleraDelTiempo :: Evento
+cremalleraDelTiempo (Barbaro "Faffy" _ _ _) = False
+cremalleraDelTiempo (Barbaro "Astro" _ _ _) = False
+cremalleraDelTiempo _                       = True
+
+-- 3)
+
+saqueo :: Evento
+saqueo unBarbaro = tieneHabilidad "Robar" unBarbaro && esFuerte unBarbaro
+
+esFuerte :: Barbaro -> Bool
+esFuerte = (>80) . fuerza
+
+
+gritoDeGuerra :: Evento
+gritoDeGuerra unBarbaro = poderDeGrito unBarbaro >= cantidadLetrasHabilidades unBarbaro
+
+cantidadLetrasHabilidades :: Barbaro -> Int
+cantidadLetrasHabilidades = sum . map length . habilidades
+
+poderDeGrito :: Barbaro -> Int
+poderDeGrito = (4*) . length . objetos
+
+caligrafia :: Evento
+caligrafia = all caligrafiaPerfecta . habilidades
+
+caligrafiaPerfecta :: Habilidad -> Bool
+caligrafiaPerfecta unaHabilidad = isUpper (head unaHabilidad) && cantidadVocales unaHabilidad > 3
+
+esVocal :: Char -> Bool
+esVocal letra = elem letra "aeiouAEIOU"
+
+cantidadVocales :: String -> Int
+cantidadVocales [] = 0
+cantidadVocales (x:xs) 
+  | esVocal x = 1 + cantidadVocales xs
+  | otherwise = 0 + cantidadVocales xs
+
+dave = Barbaro "Dave" 100 ["tejer","escribirPoesia"] [ardilla, varitasDefectuosas]
+
+
+ritualFechorias :: Barbaro -> Aventura -> Bool
+ritualFechorias unBarbaro = any (\evento -> evento unBarbaro) 
+
+
+sobrevivientes :: [Barbaro] -> Aventura -> [Barbaro]
+sobrevivientes listaBarbaros unaAventura = filter (sobrevivenA unaAventura) listaBarbaros
+
+
+sobrevivenA :: Aventura -> Barbaro -> Bool
+sobrevivenA unaAventura unBarbaro = all (\evento -> evento unBarbaro) unaAventura
 
