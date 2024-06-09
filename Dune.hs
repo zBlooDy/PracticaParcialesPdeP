4249df607b009561e2081b24c38ecfdccf2d8f55
Thiago Perez Giacchetta <tperezgiacchetta@frba.utn.edu.ar>
2024-06-09 12:53:44 -0300

    Punto 3
    Former-commit-id: e0695e51a535763dc1ae9d765ed72981901f4fed

--- Dune.hs
+++ Dune.hs
@@ -3,16 +3,21 @@
 mapReconocimiento :: (Int -> Int) -> Fremen -> Fremen
 mapReconocimiento f unFremen = unFremen {reconocimiento = f $ reconocimiento unFremen}
 
+mapTolerancia :: (Float -> Float) -> Fremen -> Fremen
+mapTolerancia f unFremen = unFremen {nivelTolerancia = f $ nivelTolerancia unFremen}
+
+mapTitulo :: ([Titulo] -> [Titulo]) -> Fremen -> Fremen
+mapTitulo f unFremen = unFremen {titulos = f $ titulos unFremen}
 -----------
 --Punto 1--
 -----------
 
 data Fremen = Fremen {
     nombre :: String,
-    nivelTolerancia :: Int,
+    nivelTolerancia :: Float,
     titulos :: [Titulo],
     reconocimiento :: Int
-} 
+} deriving (Eq)
 
 type Titulo = String
 
@@ -81,3 +86,39 @@ listaDeCrias' gusanos1 gusanos2 = map (uncurry reproducir) (zip gusanos1 gusanos
 -- uncurry le aplica a un par ordenado, la funcion que le mando
 -- entonces transformo la lista con el map, aplicandole a cada elemento (par ordenado) uncurry reproducir
 
+-----------
+--Punto 3--
+-----------
+
+type Mision = Gusano -> Fremen -> Fremen
+
+domarGusano :: Mision
+domarGusano unGusano unFremen
+  | puedeDomarlo = aumentarTolerancia 100 . ganarTitulo ("Domador") $ unFremen
+  | otherwise    = bajarTolerancia (0.1 * nivelTolerancia unFremen) unFremen
+  where
+    puedeDomarlo = (nivelTolerancia unFremen >= longitud unGusano)
+
+aumentarTolerancia :: Float -> Fremen -> Fremen
+aumentarTolerancia unaTolerancia = mapTolerancia (unaTolerancia +)
+
+bajarTolerancia :: Float -> Fremen -> Fremen
+bajarTolerancia unaTolerancia = mapTolerancia (subtract unaTolerancia)
+
+ganarTitulo :: Titulo -> Fremen -> Fremen
+ganarTitulo unTitulo = mapTitulo (unTitulo :)
+
+
+destruirGusano :: Mision
+destruirGusano unGusano unFremen
+  | puedeDestruirlo = aumentarTolerancia 100 . agregarReconocimiento 1 $ unFremen
+  | otherwise       = bajarTolerancia (0.2 * nivelTolerancia unFremen) unFremen
+  where
+    puedeDestruirlo = tieneTitulo "Domador" unFremen && nivelTolerancia unFremen < (longitud unGusano / 2)
+
+
+realizarMisionColectiva :: Gusano -> Tribu -> Mision -> Tribu
+realizarMisionColectiva unGusano unaTribu unaMision = map (unaMision unGusano) unaTribu
+
+cambiaElegido :: Gusano -> Tribu -> Mision -> Bool
+cambiaElegido unGusano unaTribu unaMision = hallarElegido unaTribu /= (hallarElegido (realizarMisionColectiva unGusano unaTribu unaMision))
