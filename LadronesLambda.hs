3026cd536da372c74965ba96f5f1bb91ef7a6bc6
Thiago Perez Giacchetta <tperezgiacchetta@frba.utn.edu.ar>
2024-06-10 11:14:03 -0300

    Parte 2
    Former-commit-id: 739a79badf376081c28308e3b1d2c2143dec3023

--- LadronesLambda.hs
+++ LadronesLambda.hs
@@ -1,3 +1,5 @@
+import Data.Char
+
 -----------
 --Parte 1--
 -----------
@@ -9,16 +11,18 @@ data Tesoro = Tesoro {
 
 -- A)
 
-deLujo :: Tesoro -> Bool
+type Tipo = Tesoro -> Bool
+
+deLujo :: Tipo
 deLujo unTesoro = (precio unTesoro > 1000) || (antiguedad unTesoro > 200)
 
 antiguedad :: Tesoro -> Int
 antiguedad unTesoro = 2024 - (anioDescubrimiento unTesoro)
 
-telaSucia :: Tesoro -> Bool
+telaSucia :: Tipo
 telaSucia unTesoro = precio unTesoro < 50 && (not . deLujo $ unTesoro)
 
-estandar :: Tesoro -> Bool
+estandar :: Tipo
 estandar unTesoro = (not . deLujo $ unTesoro) && (not . telaSucia $ unTesoro)
 
 -- B)
@@ -26,3 +30,47 @@ estandar unTesoro = (not . deLujo $ unTesoro) && (not . telaSucia $ unTesoro)
 valorTesoro :: Tesoro -> Int
 valorTesoro unTesoro = precio unTesoro + (2 * antiguedad unTesoro)
 
+-----------
+--Parte 2--
+-----------
+
+type Cerradura = String
+
+estaAbierta :: Cerradura -> Bool
+estaAbierta = null
+
+type Herramienta = Cerradura -> Cerradura
+-- a)
+martillo :: Herramienta
+martillo = drop 3
+
+-- b)
+llaveMaestra :: Herramienta
+llaveMaestra = take 0 
+
+-- c)
+ganzuaGancho :: Herramienta
+ganzuaGancho = filter (sinMayusculas) . drop 1
+
+sinMayusculas :: Char -> Bool
+sinMayusculas = not . isUpper
+
+ganzuaRastrillo :: Herramienta
+ganzuaRastrillo = filter (sinNumeros) . drop 1
+
+sinNumeros :: Char -> Bool
+sinNumeros = not . isDigit
+
+ganzuaRombo :: String -> Herramienta
+ganzuaRombo inscripcion = filter (sinInscripcion inscripcion) . drop 1
+
+sinInscripcion :: String -> Char -> Bool
+sinInscripcion unaInscripcion letra = not $ elem letra unaInscripcion
+
+-- d)
+tensor :: Herramienta
+tensor = map toUpper
+
+-- e)
+socotroco :: Herramienta -> Herramienta -> Herramienta
+socotroco unaHerramienta otraHerramienta = unaHerramienta .  otraHerramienta
