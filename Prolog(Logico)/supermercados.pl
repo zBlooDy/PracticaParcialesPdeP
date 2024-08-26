6bc1eaabef5744a766e87987959a0789018dcead
Thiago Perez Giacchetta <tperezgiacchetta@frba.utn.edu.ar>
2024-08-26 15:14:28 -0300

    Nuevo Parcial : Supermercados
    Former-commit-id: 1b3c94a86e8059562277bbf95f8daf5e1fba0b5c

---
+++ Prolog(Logico)/supermercados.pl
@@ -0,0 +1,56 @@
+%primeraMarca(Marca)
+primeraMarca(laSerenisima).
+primeraMarca(gallo).
+primeraMarca(vienisima).
+
+%precioUnitario(Producto,Precio)
+%donde Producto puede ser arroz(Marca), lacteo(Marca,TipoDeLacteo), salchicas(Marca,Cantidad)
+precioUnitario(arroz(gallo),25.10).
+precioUnitario(lacteo(laSerenisima,leche), 6.00).
+precioUnitario(lacteo(laSerenisima,crema), 4.00).
+precioUnitario(lacteo(gandara,queso(gouda)), 13.00).
+precioUnitario(lacteo(vacalin,queso(mozzarella)), 12.50).
+precioUnitario(salchichas(vienisima,12), 9.80).
+precioUnitario(salchichas(vienisima, 6), 5.80).
+precioUnitario(salchichas(granjaDelSol, 8), 5.10).
+
+%descuento(Producto, Descuento)
+descuento(lacteo(laSerenisima,leche), 0.20).
+descuento(lacteo(laSerenisima,crema), 0.70).
+descuento(lacteo(gandara,queso(gouda)), 0.70).
+descuento(lacteo(vacalin,queso(mozzarella)), 0.05).
+
+% %%%%%%%%%%%%%
+% %% Punto 1 %%
+% %%%%%%%%%%%%%
+
+descuento(arroz(Marca), 1.50) :-
+    producto(arroz(Marca)).
+
+descuento(salchichas(Marca, Cantidad), 0.50) :-
+    producto(salchichas(Marca, Cantidad)),
+    Marca \= vienisima.
+
+descuento(lacteo(Marca, leche), 2) :-
+    producto(lacteo(Marca, leche)).
+
+descuento(lacteo(Marca, queso(_)), 2) :-
+    producto(lacteo(Marca, queso(_))),
+    primeraMarca(Marca).
+
+descuento(Producto, Descuento) :-
+    precioUnitario(Producto, PrecioMayor),
+    forall(precioUnitario(_, OtroPrecio), PrecioMayor >= OtroPrecio),
+    Descuento is PrecioMayor * 0.05.
+
+%compro(Cliente,Producto,Cantidad)
+compro(juan,lacteo(laSerenisima,crema),2).
+
+
+% %%%%%%%%%%%%%
+% %% Cont. 1 %%
+% %%%%%%%%%%%%%
+
+producto(Producto) :-
+    precioUnitario(Producto, _).
+
